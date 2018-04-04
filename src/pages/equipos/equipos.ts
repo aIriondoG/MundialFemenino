ead18d6393460fefeb5f6037cb96e5bb5975dc6c
Adrián Iriondo González <airiondo@cic.es>
2018-04-04 13:48:26 +0200

    Añadida una barra de busqueda

--- src/pages/equipos/equipos.ts
+++ src/pages/equipos/equipos.ts
@@ -9,50 +9,19 @@ import {UserServiceProvider} from '../../providers/user-service/user-service';
   templateUrl: 'equipos.html',
 })
 export class EquiposPage {
-  
-  /*items: Array<{
-    name: string,
-    fifa_code: string,
-    puntos: number,
-    grupo: number, 
-    win:number,
-    draw:number,
-    lose:number,
-    goals: number,
-    goalsAgainst: number,
-   }>;*/
    items: any;
+   equipos: any;
+   searchQuery: string = '';
   constructor(public navCtrl: NavController, public navParams: NavParams, public userService: UserServiceProvider) {
-    /*this.items =[];
-    this.items.push({
-      name : 'Canada',
-      fifa_code: 'CAN',
-      puntos: 9,
-      grupo: 1,
-      win: 2,
-      draw: 2,
-      lose:1,
-      goals: 4,
-      goalsAgainst:3,
-    });
-    this.items.push({
-      name : 'Estados Unidos',
-      fifa_code: 'USA',
-      puntos: 19,
-      grupo: 1,
-      win: 6,
-      draw: 1,
-      lose: 0,
-      goals: 14,
-      goalsAgainst:3
-    });*/
+    
   }
   ionViewDidLoad() {
     this.userService.getTeams()
       .subscribe(
         (data) => {
           this.items = data;
-          console.log(this.items);
+          this.equipos=this.items;
+          //console.log(this.items);
           console.log("Rellenamos");
         },
         (error) => {
@@ -65,6 +34,26 @@ export class EquiposPage {
     this.navCtrl.push(EquipoDetailsPage, {
       items: items
     });
+  }
+  getItems(ev: any) {
+    // Reset items back to all of the items
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if ((val && val.trim() != '')||(val.trim()!=this.searchQuery)) {
+      if(val != this.searchQuery){
+        this.items= this.equipos;
+      }
+      this.items = this.items.filter((item) => {
+        return (item.country.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+      this.searchQuery = val;
+      
+    }else{
+      this.items = this.equipos;
+    }
   }
   
 
